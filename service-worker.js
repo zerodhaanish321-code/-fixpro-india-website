bd6d8f50a06a29a9a8590b028bac10b8ceba49d3
zerodhaanish321-code <zerodhaanish321@gmail.com>
2026-09-04 15:47:37 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ const CACHE_NAME = "fixpro-india-v2";
 const ASSETS_TO_CACHE = [
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
